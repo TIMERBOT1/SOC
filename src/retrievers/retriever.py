e215e4b260549f27d4b2c1ead7893a2c57294508
user <surniki622@gmail.com>
2024-11-28 10:11:28 +0500

    cmd app

--- src/retrievers/retriever.py
+++ src/retrievers/retriever.py
@@ -5,7 +5,6 @@ from omegaconf import OmegaConf
 class Retriever:
     
     def __init__(self, embeddings, dataset):
-        print('init')
         self.embeddings = embeddings
         self.dataset = dataset
 
